0a3c90ffdb36975d164d6e678b97a46d0cded2ee
Brian DMC Dennis <briandennis@datamachines.io>
2022-06-08 17:53:20 -0400

    Search .local/bin first, always add brew xonsh extension

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -3,7 +3,7 @@ export NONINTERACTIVE=t
 export DEBIAN_FRONTEND=noninteractive
 
 PYTHONUSERBASE=$HOME/.local /usr/bin/python3 -m pip install pipx
-PATH=$PATH:$HOME/.local/bin
+PATH=$HOME/.local/bin:$PATH
 pipx install homely
 homely add dotfiles
 unset GIT_SSH_COMMAND
